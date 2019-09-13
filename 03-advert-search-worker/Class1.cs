f472a4651dc05647d9d56a435ab5417617007f0b
Alexandre Yembo <alexandreyembo@gmail.com>
2019-09-13 12:25:11 +0100

    add search worker project

---
+++ 03-advert-search-worker/Class1.cs
@@ -0,0 +1,9 @@
+﻿using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+namespace Advert.SearchWorker
+{
+    public class Class1
+    {
+    }
+}
